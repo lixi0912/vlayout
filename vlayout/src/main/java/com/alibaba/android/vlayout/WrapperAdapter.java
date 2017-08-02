8650a448a3e5ac0380be4a10b7e9ca1787bbf33a
lixi <lixi0912@gmail.com>
2017-08-02 14:16:14 +0800

    1. update support version 2. support WrapperAdapter to quickly wrap RecyclerView.Adapter to DelegateAdapter.Adapter

---
+++ vlayout/src/main/java/com/alibaba/android/vlayout/WrapperAdapter.java
@@ -0,0 +1,108 @@
+package com.alibaba.android.vlayout;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.alibaba.android.vlayout.LayoutHelper;
+
+import java.util.List;
+
+/**
+ * @author 陈晓辉
+ * @description <>
+ * @date 2017/6/26
+ */
+public class WrapperAdapter<VH extends RecyclerView.ViewHolder> extends DelegateAdapter.Adapter<VH> {
+
+    private final RecyclerView.Adapter<VH> delegate;
+    private final LayoutHelper helper;
+
+    public WrapperAdapter(@NonNull RecyclerView.Adapter<VH> delegate, @NonNull LayoutHelper helper) {
+        this.delegate = delegate;
+        this.helper = helper;
+    }
+
+    @Override
+    public LayoutHelper onCreateLayoutHelper() {
+        return helper;
+    }
+
+    @Override
+    public boolean onFailedToRecycleView(VH holder) {
+        return delegate.onFailedToRecycleView(holder);
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return delegate.getItemViewType(position);
+    }
+
+    @Override
+    public void setHasStableIds(boolean hasStableIds) {
+        delegate.setHasStableIds(hasStableIds);
+    }
+
+    @Override
+    public void registerAdapterDataObserver(RecyclerView.AdapterDataObserver observer) {
+        delegate.registerAdapterDataObserver(observer);
+    }
+
+    @Override
+    public void unregisterAdapterDataObserver(RecyclerView.AdapterDataObserver observer) {
+        delegate.unregisterAdapterDataObserver(observer);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return delegate.getItemId(position);
+    }
+
+    @Override
+    public void onViewRecycled(VH holder) {
+        delegate.onViewRecycled(holder);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        delegate.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        delegate.onDetachedFromRecyclerView(recyclerView);
+    }
+
+    @Override
+    public void onViewAttachedToWindow(VH holder) {
+        delegate.onViewAttachedToWindow(holder);
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(VH holder) {
+        delegate.onViewDetachedFromWindow(holder);
+    }
+
+
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        return delegate.onCreateViewHolder(parent, viewType);
+    }
+
+    @Override
+    public void onBindViewHolder(VH holder, int position, List<Object> payloads) {
+        delegate.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public void onBindViewHolder(VH holder, int position) {
+        // no-op
+    }
+
+    @Override
+    public int getItemCount() {
+        return delegate.getItemCount();
+    }
+}
