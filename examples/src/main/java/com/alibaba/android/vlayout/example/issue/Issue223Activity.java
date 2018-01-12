664ddad06197c75a82f0d3ab9c8406cb44e4b4a4
lixi <lixi0912@gmail.com>
2018-01-12 17:05:49 +0800

    #223

---
+++ examples/src/main/java/com/alibaba/android/vlayout/example/issue/Issue223Activity.java
@@ -0,0 +1,11 @@
+package com.alibaba.android.vlayout.example.issue;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author lixi
+ * @date 2018/1/12
+ */
+
+public class Issue223Activity extends AppCompatActivity {
+}
